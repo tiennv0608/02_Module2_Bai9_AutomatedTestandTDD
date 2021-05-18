1be3b19734a7f0afc62421e96f1abc9771c9865f
tiennv0608 <tiennv0608@gmail.com>
2021-05-18 13:49:28 +0700

    B3

--- 03_B3_Tinhngaytieptheo/src/NextDayCalculator.java
+++ 03_B3_Tinhngaytieptheo/src/NextDayCalculator.java
@@ -1,64 +1,71 @@
 import java.util.Date;
 
 public class NextDayCalculator {
-    public static String calculateNextDay(int day, int month, int year) {
-        int nextDay = day + 1;
-        switch (month) {
-            case 1:
-            case 3:
-            case 5:
-            case 7:
-            case 8:
-            case 10:
-                if (nextDay > 31) {
-                    nextDay = 1;
-                    month += 1;
-                }
-                break;
-            case 12:
-                if (nextDay > 31) {
-                    nextDay = 1;
-                    month = 1;
-                    year += 1;
-                }
-                break;
-            case 4:
-            case 6:
-            case 9:
-            case 11:
-                if (nextDay > 30) {
-                    nextDay = 1;
-                    month += 1;
-                }
-                break;
-            case 2:
-                boolean isLeapYear = false;
-                boolean isDivisibleBy4 = year % 4 == 0;
-                if (isDivisibleBy4) {
-                    boolean isDivisibleBy100 = year % 100 == 0;
-                    if (isDivisibleBy100) {
-                        boolean isDivisibleBy400 = year % 400 == 0;
-                        if (isDivisibleBy400) {
+    public String calculateNextDay(int day, int month, int year) {
+        String result = "";
+        if (day>31||day<0){
+            throw new RuntimeException();
+        } else {
+            int nextDay = day + 1;
+            switch (month) {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                    if (nextDay > 31) {
+                        nextDay = 1;
+                        month += 1;
+                    }
+                    break;
+                case 12:
+                    if (nextDay > 31) {
+                        nextDay = 1;
+                        month = 1;
+                        year += 1;
+                    }
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    if (nextDay > 30) {
+                        nextDay = 1;
+                        month += 1;
+                    }
+                    break;
+                case 2:
+                    boolean isLeapYear = false;
+                    boolean isDivisibleBy4 = year % 4 == 0;
+                    if (isDivisibleBy4) {
+                        boolean isDivisibleBy100 = year % 100 == 0;
+                        if (isDivisibleBy100) {
+                            boolean isDivisibleBy400 = year % 400 == 0;
+                            if (isDivisibleBy400) {
+                                isLeapYear = true;
+                            }
+                        } else {
                             isLeapYear = true;
                         }
+                    }
+                    if (!isLeapYear) {
+                        if (nextDay > 28) {
+                            nextDay = 1;
+                            month += 1;
+                        }
                     } else {
-                        isLeapYear = true;
+                        if (nextDay > 29) {
+                            nextDay = 1;
+                            month += 1;
+                        }
                     }
-                }
-                if (!isLeapYear) {
-                    if (nextDay > 28) {
-                        nextDay = 1;
-                        month += 1;
-                    }
-                } else {
-                    if (nextDay > 29) {
-                        nextDay = 1;
-                        month += 1;
-                    }
-                }
-                break;
+                    break;
+                default:
+                    throw new RuntimeException();
+            }
+            result = nextDay + "/" + month + "/" + year;
         }
-        String result = nextDay + "/" + month + "/" + year;
         return result;
     }
 }
--- 03_B3_Tinhngaytieptheo/test/NextDayCalculatorTest.java
+++ 03_B3_Tinhngaytieptheo/test/NextDayCalculatorTest.java
@@ -8,74 +8,106 @@ import java.util.Scanner;
 class NextDayCalculatorTest {
 
     @Test
-    void calculateNextDay() {
+    void testCalculateNextDay1() {
         int day = 1;
         int month = 1;
         int year = 2018;
         String expected = "2/1/2018";
-
-        String result = NextDayCalculator.calculateNextDay(day,month,year);
+        NextDayCalculator nextDayCalculator = new NextDayCalculator();
+        String result = nextDayCalculator.calculateNextDay(day, month, year);
         assertEquals(expected, result);
     }
 
     @Test
-    void calculateNextDayLastMonthHas31Days() {
+    void testCalculateNextDay2() {
         int day = 31;
         int month = 1;
         int year = 2018;
         String expected = "1/2/2018";
-
-        String result = NextDayCalculator.calculateNextDay(day,month,year);
+        NextDayCalculator nextDayCalculator = new NextDayCalculator();
+        String result = nextDayCalculator.calculateNextDay(day, month, year);
         assertEquals(expected, result);
     }
+
     @Test
-    void calculateNextDayLastMonthHas30Days() {
+    void testCalculateNextDay3() {
         int day = 30;
         int month = 4;
         int year = 2018;
         String expected = "1/5/2018";
-
-        String result = NextDayCalculator.calculateNextDay(day,month,year);
+        NextDayCalculator nextDayCalculator = new NextDayCalculator();
+        String result = nextDayCalculator.calculateNextDay(day, month, year);
         assertEquals(expected, result);
     }
+
     @Test
-    void calculateNextDayLastFebruary() {
+    void testCalculateNextDay4() {
         int day = 28;
         int month = 2;
         int year = 2018;
         String expected = "1/3/2018";
-
-        String result = NextDayCalculator.calculateNextDay(day,month,year);
+        NextDayCalculator nextDayCalculator = new NextDayCalculator();
+        String result = nextDayCalculator.calculateNextDay(day, month, year);
         assertEquals(expected, result);
     }
+
     @Test
-    void calculateNextDayLastFebruaryInLeapYear() {
+    void testCalculateNextDay5() {
         int day = 29;
         int month = 2;
         int year = 2020;
         String expected = "1/3/2020";
-
-        String result = NextDayCalculator.calculateNextDay(day,month,year);
+        NextDayCalculator nextDayCalculator = new NextDayCalculator();
+        String result = nextDayCalculator.calculateNextDay(day, month, year);
         assertEquals(expected, result);
     }
+
     @Test
-    void calculateNextDayFebruaryInLeapYear() {
+    void testCalculateNextDay6() {
         int day = 28;
         int month = 2;
         int year = 2020;
         String expected = "29/2/2020";
-
-        String result = NextDayCalculator.calculateNextDay(day,month,year);
+        NextDayCalculator nextDayCalculator = new NextDayCalculator();
+        String result = nextDayCalculator.calculateNextDay(day, month, year);
         assertEquals(expected, result);
     }
+
     @Test
-    void calculateNextDayLastYear() {
+    void testCalculateNextDay7() {
         int day = 31;
         int month = 12;
         int year = 2018;
         String expected = "1/1/2019";
-
-        String result = NextDayCalculator.calculateNextDay(day,month,year);
+        NextDayCalculator nextDayCalculator = new NextDayCalculator();
+        String result = nextDayCalculator.calculateNextDay(day, month, year);
         assertEquals(expected, result);
+    }
+
+    @Test
+    void testCalculateNextDay8() {
+        int day = 31;
+        int month = 13;
+        int year = 2018;
+        try {
+            NextDayCalculator nextDayCalculator = new NextDayCalculator();
+            String result = nextDayCalculator.calculateNextDay(day, month, year);
+            assertFalse(true);
+        } catch (Exception exception) {
+            assertTrue(true);
+        }
+    }
+    @Test
+    void testCalculateNextDay9() {
+        int day = 0;
+        int month = 0;
+        int year = 2018;
+        try {
+            NextDayCalculator nextDayCalculator = new NextDayCalculator();
+            String result = nextDayCalculator.calculateNextDay(day, month, year);
+            assertFalse(true);
+        } catch (Exception exception) {
+            assertTrue(true);
+        }
     }
 }
