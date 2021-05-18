082c1ebb3fdc25c74b6d3fecaa541d7878f97361
tiennv0608 <tiennv0608@gmail.com>
2021-05-18 07:59:19 +0700

    b1

---
+++ 01_B1_ThuchanhUngdungmaytindongian/src/simplecalculator/src/SimpleCalculator.java
@@ -0,0 +1,13 @@
+package simplecalculator.src;
+
+public class SimpleCalculator {
+    public static int add(int first, int second){
+        return first + second;
+    }
+
+    public static int sub(int first, int second){
+        return first - second;
+    }
+}
+
+
